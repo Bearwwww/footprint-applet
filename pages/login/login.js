3b0c0f499b5033c9f215593805010f00d5c4a9db
熊w <1652166490@qq.com>
2021-06-04 21:22:40 +0800

    fix：更新了登陆界面

--- pages/login/login.js
+++ pages/login/login.js
@@ -32,16 +32,56 @@ Page({
     })
   },
 
-  // 跳转到注册界面
+  //登陆状态
+  login: function(e){
+     // 1.判断用户输入数据的合法性：不能为空，手机号应该是11位
+    var that = this;
+    // 手机号为空
+    if (that.data.userName == '') {
+      wx.showModal({
+        title: '亲！',
+        content: '请输入用户名',
+        showCancel: false,
+        success(res) { }
+      })
+    }
+    else if (that.data.phoneNumber == '') {
+      wx.showModal({
+        title: '亲！',
+        content: '请输入手机号',
+        showCancel: false,
+        success(res) { }
+      })
+    }
+    // 密码为空
+    else if (that.data.password == '') {
+      wx.showModal({
+        title: '亲！',
+        content: '请输入密码',
+        showCancel: false,
+        success(res) { }
+      })
+    }
+    // 手机号长度为空
+    else if (that.data.phoneNumber.length != 11) {
+      wx.showModal({
+        title: '亲！',
+        content: '请输入正确的手机号',
+        showCancel: false,
+        success(res) { }
+      })
+    }
+    else {
+      console.log(that.data)}
+    },
+    // 2.把数据发送到后端
+
+    // 3.页面跳转
+      // 跳转到注册界面
   goToRegister: function () {
     wx.navigateTo({
       url: "/pages/register/register"
     })
-  },
-
-  //登陆状态
-  login: function(e){
-    
   },
 
   /**
