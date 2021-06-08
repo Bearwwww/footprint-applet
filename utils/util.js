45f344882887a98a462324d8af0a7ac13388169d
熊w <1652166490@qq.com>
2021-06-08 15:10:01 +0800

    add:增加了时间戳转换工具

--- utils/util.js
+++ utils/util.js
@@ -6,14 +6,29 @@ const formatTime = date => {
   const minute = date.getMinutes()
   const second = date.getSeconds()
 
-  return `${[year, month, day].map(formatNumber).join('/')} ${[hour, minute, second].map(formatNumber).join(':')}`
+  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute].map(formatNumber).join(':')
 }
 
 const formatNumber = n => {
   n = n.toString()
-  return n[1] ? n : `0${n}`
+  return n[1] ? n : '0' + n
 }
 
+// 获取当前时间的时间戳
+function getCurrTimeStamp (){
+  return Date.parse(new Date()) / 1000;
+}
+
+function timeStamp2YMD(timeStamp){
+  var time_int =  parseInt(timeStamp)*1000;
+  var date = new Date(time_int);
+  return formatTime(date);
+}
+
+// 在XXX.js通过 var util = require("../../utils/utils.js")和
+// util.XXX()来引用
 module.exports = {
-  formatTime
+  formatTime: formatTime,
+  getCurrTimeStamp: getCurrTimeStamp,
+  timeStamp2YMD:timeStamp2YMD
 }
