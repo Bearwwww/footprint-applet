49b9693bb4ff6127ac3af0ce1ef2d8b968394dc8
git <2917021186@qq.com>
2021-06-09 10:44:37 +0800

    add:components

---
+++ components/bottom_love/bottom_love.js
@@ -0,0 +1,26 @@
+// components/bottom_love/bottom_love.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    totalLike:0,
+    commentNum:0,
+    shareNum:0,
+    collectNum:0
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
